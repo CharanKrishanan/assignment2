98932fd5747e435aa9de2b91febd367d531cd6d6
CharanKrishnan <charankovvali@gmail.com>
2023-10-05 22:00:02 +0530

    Add project files.

---
+++ assignment2/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2
+{
+    internal class Product
+    {
+        private static int nextProductId = 1;
+        public int _productid;
+        public string _productname;
+        public object _productprice;
+        public string _unitofmeasure;
+        public int _qty;
+       
+        public int Productid { get { return _productid; } set { _productid = value; } }
+        public string Productname { get { return _productname;} set { _productname = value; } }
+        public object Productprice { get { return _productprice; } set { _productprice = value; } }
+        public string Unitofmeasurement {  get { return _unitofmeasure;} set {  _unitofmeasure = value; } }
+        public int Quantity
+        {
+            get { return _qty; }
+            set { _qty = value; }
+        }
+
+        public Product()
+        {
+            
+            _productid = nextProductId;
+            nextProductId++; 
+        }
+    }
+}
---
+++ assignment2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2
+{
+    internal class Program
+    {
+        
+    static void Main(string[] args)
+      {
+        //int[] arr = { 1, 2, 3, 4, 5 };
+        //int sum = 0;
+        //for (int i = 0; i < arr.Length; i++)
+        //{
+        //    sum = sum+arr[i];
+
+        //}
+
+        //Console.WriteLine(sum);
+        
+
+            Product product1 = new Product
+            {
+                
+                _productname = "Widget",
+                _productprice = 10.99m,
+                _unitofmeasure = "Each",
+                _qty = 100
+            };
+            Product product2 = new Product
+            {
+                
+                _productname = "Gadget",
+                _productprice = 19.99m,
+                _unitofmeasure = "Piece",
+                _qty = 50
+            };
+
+            Product product3 = new Product
+            {
+                
+                _productname = "Tool",
+                _productprice = 7.49m,
+                _unitofmeasure = "Each",
+                _qty = 75
+            };
+
+            Product product4 = new Product
+            {
+                
+                _productname = "Appliance",
+                _productprice = 299.99m,
+                _unitofmeasure = "Piece",
+                _qty = 20
+            };
+
+            Product product5 = new Product
+            {
+                
+                _productname = "Accessory",
+                _productprice = 4.99m,
+                _unitofmeasure = "Each",
+                _qty = 200
+            };
+
+
+            Console.WriteLine($"Product ID: {product1._productid}");
+            Console.WriteLine($"Product Name: {product1._productname}");
+            Console.WriteLine($"Productprice: ${product1._productprice}");
+            Console.WriteLine($"Unit of Measurement: {product1._unitofmeasure}");
+            Console.WriteLine($"Quantity: {product1._qty}");
+            Console.WriteLine("--------------------------");
+
+            Console.WriteLine($"Product ID: {product2._productid}");
+            Console.WriteLine($"Product Name: {product2._productname}");
+            Console.WriteLine($"Productprice: ${product2._productprice}");
+            Console.WriteLine($"Unit of Measurement: {product2._unitofmeasure}");
+            Console.WriteLine($"Quantity: {product2._qty}");
+            Console.WriteLine("--------------------------");
+
+            Console.WriteLine($"Product ID: {product3._productid}");
+            Console.WriteLine($"Product Name: {product3._productname}");
+            Console.WriteLine($"Productprice: ${product3._productprice}");
+            Console.WriteLine($"Unit of Measurement: {product3._unitofmeasure}");
+            Console.WriteLine($"Quantity: {product3._qty}");
+            Console.WriteLine("--------------------------");
+
+            Console.WriteLine($"Product ID: {product4._productid}");
+            Console.WriteLine($"Product Name: {product4._productname}");
+            Console.WriteLine($"Productprice: ${product4._productprice}");
+            Console.WriteLine($"Unit of Measurement: {product4._unitofmeasure}");
+            Console.WriteLine($"Quantity: {product4._qty}");
+            Console.WriteLine("--------------------------");
+
+            Console.WriteLine($"Product ID: {product5._productid}");
+            Console.WriteLine($"Product Name: {product5._productname}");
+            Console.WriteLine($"Productprice: ${product5._productprice}");
+            Console.WriteLine($"Unit of Measurement: {product5._unitofmeasure}");
+            Console.WriteLine($"Quantity: {product5._qty}");
+            Console.Read();
+
+
+        }
+    }
+}
